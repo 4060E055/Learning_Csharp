25e6aaa6a2483f0aa02794d76112b449133988f6
75147514 <pt.pd08>
2022-11-30 16:53:27 +0800

    加入專案檔案。

---
+++ serialport2/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace serialport2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Connect_button = new System.Windows.Forms.Button();
+            this.buadrate_comboBox = new System.Windows.Forms.ComboBox();
+            this.port_comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Relay = new System.Windows.Forms.GroupBox();
+            this.Show_receive_message = new System.Windows.Forms.TextBox();
+            this.SendCommend = new System.Windows.Forms.Button();
+            this.HIGH_button = new System.Windows.Forms.Button();
+            this.LOW_button = new System.Windows.Forms.Button();
+            this.Disconnect_button = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Setting_button = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.mo_textBox = new System.Windows.Forms.TextBox();
+            this.product_name_textBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Relay.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Connect_button
+            // 
+            this.Connect_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Connect_button.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Connect_button.Location = new System.Drawing.Point(226, 43);
+            this.Connect_button.Name = "Connect_button";
+            this.Connect_button.Size = new System.Drawing.Size(78, 21);
+            this.Connect_button.TabIndex = 1;
+            this.Connect_button.Text = "Connect";
+            this.Connect_button.UseVisualStyleBackColor = false;
+            this.Connect_button.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // buadrate_comboBox
+            // 
+            this.buadrate_comboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buadrate_comboBox.FormattingEnabled = true;
+            this.buadrate_comboBox.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "57600"});
+            this.buadrate_comboBox.Location = new System.Drawing.Point(79, 80);
+            this.buadrate_comboBox.Name = "buadrate_comboBox";
+            this.buadrate_comboBox.Size = new System.Drawing.Size(121, 21);
+            this.buadrate_comboBox.TabIndex = 24;
+            this.buadrate_comboBox.Text = "9600";
+            // 
+            // port_comboBox
+            // 
+            this.port_comboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.port_comboBox.FormattingEnabled = true;
+            this.port_comboBox.Location = new System.Drawing.Point(79, 43);
+            this.port_comboBox.Name = "port_comboBox";
+            this.port_comboBox.Size = new System.Drawing.Size(121, 21);
+            this.port_comboBox.TabIndex = 11;
+            this.port_comboBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.port_comboBox_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Buadrate：";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Relay
+            // 
+            this.Relay.Controls.Add(this.Disconnect_button);
+            this.Relay.Controls.Add(this.buadrate_comboBox);
+            this.Relay.Controls.Add(this.label2);
+            this.Relay.Controls.Add(this.Connect_button);
+            this.Relay.Controls.Add(this.label1);
+            this.Relay.Controls.Add(this.port_comboBox);
+            this.Relay.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Relay.Location = new System.Drawing.Point(29, 37);
+            this.Relay.Name = "Relay";
+            this.Relay.Size = new System.Drawing.Size(324, 129);
+            this.Relay.TabIndex = 5;
+            this.Relay.TabStop = false;
+            this.Relay.Text = "Connect";
+            // 
+            // Show_receive_message
+            // 
+            this.Show_receive_message.Location = new System.Drawing.Point(6, 50);
+            this.Show_receive_message.Multiline = true;
+            this.Show_receive_message.Name = "Show_receive_message";
+            this.Show_receive_message.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Show_receive_message.Size = new System.Drawing.Size(215, 95);
+            this.Show_receive_message.TabIndex = 8;
+            // 
+            // SendCommend
+            // 
+            this.SendCommend.Enabled = false;
+            this.SendCommend.Location = new System.Drawing.Point(227, 22);
+            this.SendCommend.Name = "SendCommend";
+            this.SendCommend.Size = new System.Drawing.Size(126, 28);
+            this.SendCommend.TabIndex = 3;
+            this.SendCommend.Text = "Send Commend";
+            this.SendCommend.UseVisualStyleBackColor = true;
+            this.SendCommend.Click += new System.EventHandler(this.SendCommend_Click);
+            // 
+            // HIGH_button
+            // 
+            this.HIGH_button.Enabled = false;
+            this.HIGH_button.Location = new System.Drawing.Point(94, 22);
+            this.HIGH_button.Name = "HIGH_button";
+            this.HIGH_button.Size = new System.Drawing.Size(61, 23);
+            this.HIGH_button.TabIndex = 11;
+            this.HIGH_button.Text = "HIGH";
+            this.HIGH_button.UseVisualStyleBackColor = true;
+            this.HIGH_button.Click += new System.EventHandler(this.HIGH_Click);
+            // 
+            // LOW_button
+            // 
+            this.LOW_button.Enabled = false;
+            this.LOW_button.Location = new System.Drawing.Point(16, 22);
+            this.LOW_button.Name = "LOW_button";
+            this.LOW_button.Size = new System.Drawing.Size(61, 21);
+            this.LOW_button.TabIndex = 11;
+            this.LOW_button.Text = "LOW";
+            this.LOW_button.UseVisualStyleBackColor = true;
+            this.LOW_button.Click += new System.EventHandler(this.LOW_Click);
+            // 
+            // Disconnect_button
+            // 
+            this.Disconnect_button.Enabled = false;
+            this.Disconnect_button.Location = new System.Drawing.Point(226, 80);
+            this.Disconnect_button.Name = "Disconnect_button";
+            this.Disconnect_button.Size = new System.Drawing.Size(78, 23);
+            this.Disconnect_button.TabIndex = 11;
+            this.Disconnect_button.Text = "Disconnect";
+            this.Disconnect_button.UseVisualStyleBackColor = true;
+            this.Disconnect_button.Click += new System.EventHandler(this.Disconnect_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Commend";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Setting_button
+            // 
+            this.Setting_button.Location = new System.Drawing.Point(253, 56);
+            this.Setting_button.Name = "Setting_button";
+            this.Setting_button.Size = new System.Drawing.Size(100, 23);
+            this.Setting_button.TabIndex = 4;
+            this.Setting_button.Text = "Auto setting";
+            this.Setting_button.UseVisualStyleBackColor = true;
+            this.Setting_button.Click += new System.EventHandler(this.Setting_button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Get DC";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // mo_textBox
+            // 
+            this.mo_textBox.Location = new System.Drawing.Point(89, 22);
+            this.mo_textBox.Name = "mo_textBox";
+            this.mo_textBox.Size = new System.Drawing.Size(100, 23);
+            this.mo_textBox.TabIndex = 7;
+            this.mo_textBox.Text = "011017B0";
+            this.mo_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.mo_textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mo_textBox_KeyDown);
+            // 
+            // product_name_textBox
+            // 
+            this.product_name_textBox.Location = new System.Drawing.Point(89, 66);
+            this.product_name_textBox.Name = "product_name_textBox";
+            this.product_name_textBox.Size = new System.Drawing.Size(100, 23);
+            this.product_name_textBox.TabIndex = 8;
+            this.product_name_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LOW_button);
+            this.groupBox1.Controls.Add(this.HIGH_button);
+            this.groupBox1.Location = new System.Drawing.Point(387, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 71);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Relay";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.Setting_button);
+            this.groupBox2.Controls.Add(this.Show_receive_message);
+            this.groupBox2.Controls.Add(this.SendCommend);
+            this.groupBox2.Location = new System.Drawing.Point(381, 142);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(369, 155);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Send Commend";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.mo_textBox);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.product_name_textBox);
+            this.groupBox3.Location = new System.Drawing.Point(29, 187);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(346, 120);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "SQL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "工單：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "機種名稱：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 461);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Relay);
+            this.Name = "Form1";
+            this.Text = "Relay";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Relay.ResumeLayout(false);
+            this.Relay.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button Connect_button;
+        private System.Windows.Forms.ComboBox buadrate_comboBox;
+        private System.Windows.Forms.ComboBox port_comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox Relay;
+        private System.Windows.Forms.Button Disconnect_button;
+        private System.Windows.Forms.Button LOW_button;
+        private System.Windows.Forms.Button HIGH_button;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button SendCommend;
+        private System.Windows.Forms.TextBox Show_receive_message;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Setting_button;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox mo_textBox;
+        private System.Windows.Forms.TextBox product_name_textBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ serialport2/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using MySql.Data.MySqlClient;
+
+namespace serialport2
+{
+    public partial class Form1 : Form
+    {
+        public class Global
+        {
+            public static int number = 0;
+            public static int Number { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (port_comboBox.Text == "" || buadrate_comboBox.Text == "")//no setting status
+                {
+                    MessageBox.Show(" 未選擇 Arduino 通訊埠 或 Buadrate !!");
+                    return;
+                }
+                else if (serialPort1.IsOpen)// connected status
+                {
+                    serialPort1.Dispose();
+                    connect_port();
+                    // MessageBox.Show("Linking again is " + serialPort1.IsOpen.ToString());
+                }
+                else if ((!serialPort1.IsOpen))// no error status
+                {
+                    connect_port();
+                    // MessageBox.Show("Linking is" + serialPort1.IsOpen.ToString());
+                }
+
+            }
+            catch (Exception a)
+            {
+
+                Console.WriteLine("{0} Exception caught.", a);
+            }
+
+        }
+
+        private void connect_port()
+        {
+            set_serialPort();
+            serialPort1.Open();//connect
+            connect_style();
+        }
+
+        private void connect_style()
+        {
+            Connect_button.Text = "Connected";
+            Connect_button.BackColor = Color.GreenYellow;
+            Disconnect_button.Enabled = true;
+            LOW_button.Enabled = true;
+            HIGH_button.Enabled = true;
+            SendCommend.Enabled = true;
+        }
+
+        private void set_serialPort()
+        {
+            serialPort1.PortName = port_comboBox.Text.ToString();
+            serialPort1.BaudRate = int.Parse(buadrate_comboBox.Text);
+        }
+
+        private void port_comboBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            find_all_ports();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            find_all_ports();
+        }
+
+        private void find_all_ports()
+        {
+            port_comboBox.Items.Clear();
+            string[] serialPorts = System.IO.Ports.SerialPort.GetPortNames();
+
+            if (serialPorts.Length != 0)
+            {
+                port_comboBox.Text = serialPorts[0];
+            }
+            else
+            {
+                port_comboBox.Text = "";
+                MessageBox.Show("Not found any port number.\nPlease confirm the USB.");
+            }
+
+            port_comboBox.Items.AddRange(serialPorts);
+        }
+
+        private void Disconnect_button_Click(object sender, EventArgs e)
+        {
+            serialPort1.Dispose();//disconnect
+            //MessageBox.Show("Disconnect is " + serialPort1.IsOpen.ToString());
+            disconnect_style();
+        }
+
+        private void disconnect_style()
+        {
+            Connect_button.Text = "Connect";
+            Connect_button.BackColor = Color.FromArgb(224, 224, 224);
+            Disconnect_button.Enabled = false;
+            HIGH_button.Enabled = false;
+            LOW_button.Enabled = false;
+            SendCommend.Enabled = false;
+        }
+
+        private void LOW_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("LOW3\r");
+        }
+
+
+        private void HIGH_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("HIGH3\r\n");
+        }
+
+        private async void SendCommend_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(textBox1.Text + "\r\n");//send message
+            await Task.Delay(100);
+            Global.Number += 1;
+            string receivedata = get_receive_message();
+            Show_receive_message.AppendText(Global.Number + ":  " + receivedata + "  " + "\r\n");
+
+        }
+        private string get_receive_message()
+        {
+
+            int len = serialPort1.BytesToRead;
+            string receivedata = String.Empty;
+            if (len != 0)
+            {
+                receivedata = serialPort1.ReadExisting();
+
+                //這方法就算delay還是會導致部分字串收不到
+                //byte[] buff = new byte[len];
+                //serialPort1.ReadExisting();
+                //receivedata = Encoding.Default.GetString(buff);
+            }
+            return receivedata;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Show_receive_message.Text = "";
+            Global.Number = 0;
+        }
+
+        private async void Setting_button_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("OUTP:STAT:IMM ON" + "\r\n" + "APPL 10,5" + "\r\n");
+            await Task.Delay(100);
+            Show_receive_message.Text = "It's OK.\r\n";
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            //double[] dc = new double[5];
+            double dc = 0.0;
+            double sum_dc = 0.0f;
+
+            for (int i = 0; i < 5; i++)
+            {
+                serialPort1.Write("MEAS:SCAL:CURR:DC?" + "\r\n");
+                await Task.Delay(100);
+                dc = Convert.ToDouble(get_receive_message().Substring(1)) * 1000;
+                sum_dc += dc;
+                Show_receive_message.AppendText((i + 1) + ": " + dc.ToString() + "\r\n");
+            }
+
+            Show_receive_message.AppendText("Average： " + (sum_dc / 5).ToString());
+        }
+
+
+        private void mo_textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                if (mo_textBox.Text == "")
+                {
+                    MessageBox.Show("Please input the 工單.");
+
+                }
+                else
+                {
+                    String production_name = query_database();
+                    product_name_textBox.Text = production_name;
+                }
+            }
+        }
+
+        private string query_database()
+        {
+
+            string Connect_Info = setting_info_for_connect();
+
+            MySqlConnection connect_database = new MySqlConnection(Connect_Info);
+           
+            //------Connect---------------------------------------
+            try
+            {
+                connect_database.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("無法連線到資料庫.");
+                        MessageBox.Show("Could not connect to database.");
+                        break;
+                    case 1045:
+                        Console.WriteLine("The user account or password is wrong, please try again.");
+                        break;
+                }
+            }
+            //-----------------------------------------------------------
+
+
+            //----------Quary----------------------------
+            String cmdText =
+                "SELECT product_name " +
+                "FROM Production_Information " +
+                "WHERE mo='" + mo_textBox.Text + "'";
+            //String cmdText = "SELECT product_name FROM Production_Information WHERE mo='" + mo_textBox.Text + "'";
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(cmdText, connect_database);
+                MySqlDataReader myData = cmd.ExecuteReader();
+
+                if (!myData.HasRows)
+                {
+                    // 如果沒有資料,顯示沒有資料的訊息
+                    //Console.WriteLine("No data.");
+                    MessageBox.Show("No data.");
+                }
+                else
+                {
+                    // 讀取資料並且顯示出來
+                    while (myData.Read())
+                    {
+                        //Console.WriteLine("Text={0}", myData.GetString(0));
+                        MessageBox.Show("讀取的資料Text={0}", myData.GetString(0));
+                        return myData.GetString(0);
+                    }
+                    myData.Close();
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                //Console.WriteLine("Error " + ex.Number + " : " + ex.Message);
+                MessageBox.Show("Error " + ex.Number + " : " + ex.Message);
+            }
+            //---------------------------------
+
+            //MySqlDataAdapter dat = new MySqlDataAdapter();
+            //DataTable dt = new DataTable();
+            //dat.SelectCommand = cmd;
+            //dat.Fill(dt);
+            //dat.Dispose();
+
+            //connect_database.Close();
+
+            return "error";
+        }
+
+        private string setting_info_for_connect()
+        {
+            string sql_server = "192.168.0.99";
+            string sql_UID = "johnson";
+            string sql_password = "johnson";
+            string Connect_Info =
+                "Server=" + sql_server + ";" +
+                "Database=ma430104_Production_Information;" +
+                "uid=" + sql_UID + ";" +
+                "Pwd=" + sql_password + ";" +
+                "charset=utf8;" +
+                "SslMode=None;";
+
+
+
+            return Connect_Info;
+        }
+    }
+}
